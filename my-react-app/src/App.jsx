69187a455092b41571c07fe9f469dd9be3fe9286
Asad Zaman <10asadzaman@gmail.com>
2025-09-23 21:09:17 +0600

    learning useState

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,15 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Counter from './Counter'
 
 function App() {
 
-const handleClick = (num) => {
-  alert(`I am clicked ${num}`)
-}
   return (
     <>
-    <button onClick={() => {handleClick(700)}}>Click Me</button>
+    <Counter>
+
+    </Counter>
     </>
   )
 }
