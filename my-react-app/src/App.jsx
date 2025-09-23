4875a75083d1a3f85598c5ffd81f383f84ae368d
Asad Zaman <10asadzaman@gmail.com>
2025-09-24 02:12:55 +0600

    explored useState

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,16 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Counter from './Counter'
+import Runs from './Runs'
 
 function App() {
 
-  return (
-    <>
-    <Counter>
 
-    </Counter>
-    </>
+
+
+  return (
+   <Runs></Runs>
   )
 }
 
