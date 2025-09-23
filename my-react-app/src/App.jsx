f0c8e8c2c616da673a6c6047eac8bff12226e480
Asad Zaman <10asadzaman@gmail.com>
2025-09-23 17:09:23 +0600

    button testing

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -4,30 +4,19 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+
+function handleClick(){
+  alert('I am clicked')
+}
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
+     <button onClick={handleClick}>Click ME</button>
+     <button onClick={function handleClick2(){
+      alert('CLick 2 DOne')
+     }}>Click ME 2</button>
     </>
   )
 }
