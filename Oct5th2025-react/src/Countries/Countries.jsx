4926978d7a64a07410bcda0bb22452a0d73cbd57
Asad Zaman <10asadzaman@gmail.com>
2025-10-06 05:17:54 +0600

    added list in country

--- Oct5th2025-react/src/Countries/Countries.jsx
+++ Oct5th2025-react/src/Countries/Countries.jsx
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { use } from 'react';
 import Country from './Country';
-import './Countries.jsx'
 import './Countries.css'
-const Countries = ({countriesPromise}) => {
+const Countries = ({ countriesPromise }) => {
     const countriesData = use(countriesPromise);
     const countries = countriesData.countries;
+    const [visitedCountries, setVisitedCountries] = useState([]);
+
+    const handleVisitedCountries = (country) => {
+        const newVisitedCountry = [...visitedCountries, country];
+        setVisitedCountries(newVisitedCountry);
+    }
     return (
         <div>
             <h1>Total Countries: {countries.length}</h1>
+            <h2>Total Visited Countries: {visitedCountries.length}</h2>
+            <ol>
+                {visitedCountries.map(
+                    country => <li key={country.cca3.cca3}>{country.name.common}</li>
+                )}
+            </ol>
             <div className="countries">
-             {countries.map(country => <Country country={country}></Country>)}
+                {countries.map(country => <Country key={country.cca3.cca3} handleVisitedCountries={handleVisitedCountries} country={country}></Country>)}
             </div>
-           
+
         </div>
     );
 };
