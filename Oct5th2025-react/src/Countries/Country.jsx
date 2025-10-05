8d4aa55217db6e313e08ec9f887a59ac9d1a67ec
Asad Zaman <10asadzaman@gmail.com>
2025-10-06 00:17:35 +0600

    css applied when not visited button clicked

--- Oct5th2025-react/src/Countries/Country.jsx
+++ Oct5th2025-react/src/Countries/Country.jsx
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Country.css'
-const Country = ({country}) => {
-   console.log(country)
+const Country = ({ country }) => {
+    console.log(country)
+    const [visited, setVisited] = useState(false);
+    const handleVisited = () => {
+        // // system 1
+        // if(visited){
+        //     setVisited(false)
+        // }
+        // else{
+        //     setVisited(true)
+        // }
+
+        // // system 2
+        // setVisited(visited ? false : true)
+
+        // system 3
+        setVisited(!visited)
+    }
     return (
-        <div className='country'>
-           <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
-           <h1>Country: {country.name.common}</h1>
-           <h2>Capital: {country.capital.capital[0]}</h2>
-           <h2>Population: {country.population.population}</h2>
-           <h2>Region: {country.region.region}</h2>
+        <div className={`country ${visited && 'country-visited'}` }>
+            <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
+            <h1>Country: {country.name.common}</h1>
+            <h2>Capital: {country.capital.capital[0]}</h2>
+            <h2>Population: {country.population.population}</h2>
+            <h2>Region: {country.region.region}</h2>
+            <button onClick={handleVisited}>{visited ? 'Visited' : 'Not Visited'}</button>
         </div>
     );
 };
