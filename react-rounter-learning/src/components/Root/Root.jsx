be66d5754915880e9273513d93503db30f7c6bae
Asad Zaman <10asadzaman@gmail.com>
2025-10-30 15:13:01 +0600

    learned to use useNavigator()

--- react-rounter-learning/src/components/Root/Root.jsx
+++ react-rounter-learning/src/components/Root/Root.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import Header from '../Header/Header';
-import { Outlet } from 'react-router';
+import { Outlet, useNavigation } from 'react-router';
 import Footer from '../Footer/Footer';
 import Sidebar from '../Sidebar/Sidebar';
 import './Root.css'
 
 const Root = () => {
+    const navigation = useNavigation();
+    const isNavigating = Boolean(navigation.location)
     return (
         <div>
             <Header></Header>
             <div className='main-root'>
                 <Sidebar></Sidebar>
+                {isNavigating && <h1>Loading....</h1>}
                 <Outlet></Outlet>
             </div>
 
