a5e2741eef88cd08fce9285ff7ac296937bcf88b
Asad Zaman <10asadzaman@gmail.com>
2025-10-30 13:52:31 +0600

    learned the usage of useNavigate routing feature

--- react-rounter-learning/src/components/Post/Post.jsx
+++ react-rounter-learning/src/components/Post/Post.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import './Post.css'
-import { Link } from 'react-router';
+import { Link, useNavigate } from 'react-router';
 
 const Post = ({post}) => {
     const {body, id , title , userID} = post;
+    let navigate = useNavigate();
+   
     return (
         <div className='post'>
             <h2>{title}</h2>
             <Link to={`/posts/${id}`}>
             <button>See full post</button></Link>
+            <button onClick={() => navigate(`/posts/${id}`)}>See Post {id}</button>
         </div>
     );
 };
--- react-rounter-learning/src/components/PostDetails/PostdDetails.jsx
+++ react-rounter-learning/src/components/PostDetails/PostdDetails.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import { useLoaderData } from 'react-router';
+import { useLoaderData, useNavigate } from 'react-router';
 import '../PostDetails/PostDetails.css'
 
 const PostdDetails = () => {
     const post = useLoaderData();
     const {body , title} = post;
+    const navigate = useNavigate();
     return (
         <div className='post'>
-            <h1>{title}</h1>
+            <h3>{title}</h3>
             <p>{body}</p>
+            <button onClick={() => navigate('/laptops')}>Main Post Section</button>
         </div>
     );
 };
