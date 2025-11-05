fb32e4e78f321b8831566ba0c1b8c2319f9493f2
Asad Zaman <10asadzaman@gmail.com>
2025-11-05 17:26:22 +0600

    explored form and context state

--- exploring-react-form/src/FamilyTree/Dad.jsx
+++ exploring-react-form/src/FamilyTree/Dad.jsx
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { use, useContext } from 'react';
 import Mom from './Mom';
 import Myself from './Myself';
 import Brother from './Brother';
 import Sister from './Sister';
+import { MoneyContext } from './FamilyTree';
 
 const Dad = ({gift}) => {
+    const [money, setMoney] = useContext(MoneyContext);
+
     return (
         <div>
             <h3>Dad</h3>
             <section className='flex'>
+                <button onClick={() => {
+                    setMoney(money + 5000)
+                }}>Add Money 5000Taka </button>
                 <Myself gift={gift}></Myself>
                 <Brother></Brother>
                 <Sister></Sister>
--- exploring-react-form/src/FamilyTree/FamilyTree.jsx
+++ exploring-react-form/src/FamilyTree/FamilyTree.jsx
@@ -1,18 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Grandpa from './Grandpa';
 import '../FamilyTree/FamilyTree.css'
 import { createContext } from 'react';
 
 export const AssetContext = createContext('');
+export const MoneyContext = createContext(0);
 
 
 const FamilyTree = () => {
     const newAsset = 'Sweets'
+    const [money, setMoney] = useState(0);
     return (
         <div className='family'>
-            <AssetContext.Provider value={newAsset}>
+            <h2>Total Family Money: {money}</h2>
+            <MoneyContext value={[money, setMoney]}>
+         <AssetContext.Provider value={newAsset}>
                 <Grandpa></Grandpa>
             </AssetContext.Provider>
+            </MoneyContext>
+           
 
         </div>
     );
