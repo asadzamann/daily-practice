74dd0deaa17bec329b6cb75ff3711aac73e434ea
Asad Zaman <10asadzaman@gmail.com>
2025-11-25 17:40:01 +0600

    feat(auth): github authorization

--- firebase-auth-react/src/App.jsx
+++ firebase-auth-react/src/App.jsx
@@ -1,45 +1,67 @@
 import { useState } from 'react'
-import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+import { GithubAuthProvider, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 
 import './App.css'
 import { auth } from './firbase/firebase.config';
 
 function App() {
-  
-  const googleProvider = new GoogleAuthProvider();
-  const [user , setUser] = useState(null)
 
-  const loginHandler = () => {
+  const googleProvider = new GoogleAuthProvider();
+  const githubProvider = new GithubAuthProvider();
+  const [user, setUser] = useState(null)
+
+  const googleLoginHandler = () => {
     signInWithPopup(auth, googleProvider)
-    .then(result => {
-      console.log('check result', result.user)
-      setUser(result.user)
-    })
-    .catch(error => {
-      console.log('check error', error)
-    })
+      .then(result => {
+        console.log('check result', result.user)
+        setUser(result.user)
+      })
+      .catch(error => {
+        console.log('check error', error)
+      })
 
   }
 
+  const githubLoginHandler = () => {
+    signInWithPopup(auth, githubProvider)
+    .then(result => {
+      console.log(result)
+      setUser(result.user)
+    })
+    .then(
+      error => {
+        console.log(error)
+      }
+    )
+  }
+
   const signoutHandler = () => {
-    
+
     signOut(auth)
-    .then(() => {
-      console.log('Signed Out Successfully');
-      setUser(null)
-    })
-    .catch(error => {
-      console.log(error)
-    })
+      .then(() => {
+        console.log('Signed Out Successfully');
+        setUser(null)
+      })
+      .catch(error => {
+        console.log(error)
+      })
   }
 
 
   return (
-    <>   
+    <>
       <h1>Firebase Auth React</h1>
-      <button onClick={loginHandler}>Login with Google</button>
-      <button onClick={signoutHandler}>Signout</button>
-     {user && <div>
+      {
+        user ? <button onClick={signoutHandler}>Signout</button> :
+          <>
+            <button onClick={googleLoginHandler}>Login with Google</button>
+            <button onClick={githubLoginHandler}>Login with GitHub</button>
+          </>
+
+      }
+
+
+      {user && <div>
         <h1>Name: {user.displayName}</h1>
         <h1>Email: {user.email}</h1>
         <img src={user.photoURL} alt="" />
