a411039b12f14586e766124a3854319bd0011703
Asad Zaman <10asadzaman@gmail.com>
2025-11-25 14:14:14 +0600

    displayed logged in user info, built sign out, implemented signin and signout toggle

--- simple-firebase-auth/src/components/Login.jsx
+++ simple-firebase-auth/src/components/Login.jsx
@@ -1,15 +1,29 @@
-import React from 'react';
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import React, { useState } from 'react';
+import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 import { auth } from '../firebase/firebase.config';
 
 
 
 const Login = () => {
+    const [user, setUser] = useState(null);
+    
+
     const googleProvider = new GoogleAuthProvider();
     const googleClickHandler = () => {
         signInWithPopup(auth, googleProvider)
         .then(result => {
-            console.log(result);
+            console.log('check result', result.user);
+            setUser(result.user)
+        })
+        .catch(error => {
+            console.log('check error', error);
+        })
+    }
+    const signoutHandler = () => {
+        signOut(auth)
+        .then(() => {
+            console.log("Sign Out Successful");
+            setUser(null)
         })
         .catch(error => {
             console.log(error)
@@ -19,6 +33,13 @@ const Login = () => {
         <div>
             <h1>Login</h1>
             <button onClick={googleClickHandler}>Sign in with Google</button>
+            <button onClick={signoutHandler}>Sign Out</button>
+            {user && <div>
+          <h2>Name: {user?.displayName}</h2>
+            <h2>Email: {user?.email}</h2>
+            <img src={user.photoURL} alt="" />
+            </div>}
+          
         </div>
     );
 };
